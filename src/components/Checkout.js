3dd117aaa2b7e62ab722e5531f0b9fb504b17351
Afolayan Stephen <afolayan4life@gmail.com>
2021-04-17 18:17:51 +0100

    Added custom scroll bar and enabled toggle for checkout

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,90 +1,90 @@
 import '../styles/Checkout.css'
 import image from "../design-files/hero-image.png"
+import {Scrollbars} from "react-custom-scrollbars"
 
 function Checkout(){
     return(
         <div className='checkout-wrapper'>
             <div className="checkout shadow-lg">
                 <h3>Checkout</h3>
-
-                <div className="checkout-item shadow-sm bg-white mb-3">
-                    <span>x</span>
-                    <img src={image} alt="Checkout item name" />
-                    <div className="item-details">
-                        <b>Grilled Chicken</b>
-                        <i>FCFA 2500</i>
-                        <input type="number" min="1" className="quantity" />
+                <Scrollbars className="checkout-item-wrapper">
+                {/* <div className="checkout-item-wrapper"> */}
+                    <div className="checkout-item shadow-sm bg-white mb-3">
+                        <span>x</span>
+                        <img src={image} alt="Checkout item name" />
+                        <div className="item-details">
+                            <b>Grilled Chicken</b>
+                            <i>FCFA 2500</i>
+                            <input type="number" min="1" className="quantity" />
+                        </div>
                     </div>
-                </div>
-
-                <div className="checkout-item shadow-sm bg-white mb-3">
-                    <span>x</span>
-                    <img src={image} alt="Checkout item name" />
-                    <div className="item-details">
-                        <b>Grilled Chicken</b>
-                        <i>FCFA 2500</i>
-                        <input type="number" min="1" className="quantity" />
+                    <div className="checkout-item shadow-sm bg-white mb-3">
+                        <span>x</span>
+                        <img src={image} alt="Checkout item name" />
+                        <div className="item-details">
+                            <b>Grilled Chicken</b>
+                            <i>FCFA 2500</i>
+                            <input type="number" min="1" className="quantity" />
+                        </div>
                     </div>
-                </div>
-
-                <div className="checkout-item shadow-sm bg-white mb-3">
-                    <span>x</span>
-                    <img src={image} alt="Checkout item name" />
-                    <div className="item-details">
-                        <b>Grilled Chicken</b>
-                        <i>FCFA 2500</i>
-                        <input type="number" min="1" className="quantity" />
+                    <div className="checkout-item shadow-sm bg-white mb-3">
+                        <span>x</span>
+                        <img src={image} alt="Checkout item name" />
+                        <div className="item-details">
+                            <b>Grilled Chicken</b>
+                            <i>FCFA 2500</i>
+                            <input type="number" min="1" className="quantity" />
+                        </div>
                     </div>
-                </div>
-
-                <div className="checkout-item shadow-sm bg-white mb-3">
-                    <span>x</span>
-                    <img src={image} alt="Checkout item name" />
-                    <div className="item-details">
-                        <b>Grilled Chicken</b>
-                        <i>FCFA 2500</i>
-                        <input type="number" min="1" className="quantity" />
+                    <div className="checkout-item shadow-sm bg-white mb-3">
+                        <span>x</span>
+                        <img src={image} alt="Checkout item name" />
+                        <div className="item-details">
+                            <b>Grilled Chicken</b>
+                            <i>FCFA 2500</i>
+                            <input type="number" min="1" className="quantity" />
+                        </div>
                     </div>
-                </div>
-
-                <div className="checkout-item shadow-sm bg-white mb-3">
-                    <span>x</span>
-                    <img src={image} alt="Checkout item name" />
-                    <div className="item-details">
-                        <b>Grilled Chicken</b>
-                        <i>FCFA 2500</i>
-                        <input type="number" min="1" className="quantity" />
+                    <div className="checkout-item shadow-sm bg-white mb-3">
+                        <span>x</span>
+                        <img src={image} alt="Checkout item name" />
+                        <div className="item-details">
+                            <b>Grilled Chicken</b>
+                            <i>FCFA 2500</i>
+                            <input type="number" min="1" className="quantity" />
+                        </div>
                     </div>
-                </div>
-
-                <div className="checkout-item shadow-sm bg-white mb-3">
-                    <span>x</span>
-                    <img src={image} alt="Checkout item name" />
-                    <div className="item-details">
-                        <b>Grilled Chicken</b>
-                        <i>FCFA 2500</i>
-                        <input type="number" min="1" className="quantity" />
+                    <div className="checkout-item shadow-sm bg-white mb-3">
+                        <span>x</span>
+                        <img src={image} alt="Checkout item name" />
+                        <div className="item-details">
+                            <b>Grilled Chicken</b>
+                            <i>FCFA 2500</i>
+                            <input type="number" min="1" className="quantity" />
+                        </div>
                     </div>
-                </div>
-
-                <div className="checkout-item shadow-sm bg-white mb-3">
-                    <span>x</span>
-                    <img src={image} alt="Checkout item name" />
-                    <div className="item-details">
-                        <b>Grilled Chicken</b>
-                        <i>FCFA 2500</i>
-                        <input type="number" min="1" className="quantity" />
+                    <div className="checkout-item shadow-sm bg-white mb-3">
+                        <span>x</span>
+                        <img src={image} alt="Checkout item name" />
+                        <div className="item-details">
+                            <b>Grilled Chicken</b>
+                            <i>FCFA 2500</i>
+                            <input type="number" min="1" className="quantity" />
+                        </div>
                     </div>
-                </div>
-
-                <div className="checkout-item shadow-sm bg-white mb-3">
-                    <span>x</span>
-                    <img src={image} alt="Checkout item name" />
-                    <div className="item-details">
-                        <b>Grilled Chicken</b>
-                        <i>FCFA 2500</i>
-                        <input type="number" min="1" className="quantity" />
+                    <div className="checkout-item shadow-sm bg-white mb-3">
+                        <span>x</span>
+                        <img src={image} alt="Checkout item name" />
+                        <div className="item-details">
+                            <b>Grilled Chicken</b>
+                            <i>FCFA 2500</i>
+                            <input type="number" min="1" className="quantity" />
+                        </div>
                     </div>
+                {/* </div> */}
+                </Scrollbars>
+                <div className="checkout-cta">
+                    <button className="checkout-btn">Checkout</button>
                 </div>
             </div>
         </div>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,21 @@
+import React, { useState } from "react"
 import {Navbar, Container, Nav, Badge} from 'react-bootstrap'
 import {FaShoppingCart, FaBars} from 'react-icons/fa';
 import "../styles/Navigation.css"
 import Checkout from './Checkout';
 
+
 function Navigation(){
+
+    const [showCheckout, setShowCheckout] = useState(false);
+    const toggleCheckout = () => {
+        if ( showCheckout ){
+            setShowCheckout(false);
+        } else {
+            setShowCheckout(true);
+        }
+    }
+
     return(
         <Navbar fixed="top" bg="transparent" expand="lg" style={{paddingTop: '35px', zIndex: 1000}}>
             <Container>
@@ -20,12 +32,12 @@ function Navigation(){
                 
                 {/* <Navbar.Collapse id="basic-navbar-nav"> */}
                     <Nav className="ml-auto nav-hack">
-                    <Nav.Link href="#cart"><FaShoppingCart className="navigation-link" /><sup><Badge pill className={'pill-style'}>276</Badge></sup></Nav.Link>
+                    <Nav.Link href="#cart" onClick={toggleCheckout}><FaShoppingCart className="navigation-link" /><sup><Badge pill className={'pill-style'}>276</Badge></sup></Nav.Link>
                         <Nav.Link href="#menu"><FaBars className="navigation-link" /></Nav.Link>
                     </Nav>
                 {/* </Navbar.Collapse> */}
             </Container>
-            <Checkout />
+            { showCheckout ? <Checkout /> : null }
         </Navbar>
     )
 }
