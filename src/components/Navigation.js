9d0e037897d1bb3a2b892e645c876204369ee70a
Afolayan Stephen <afolayan4life@gmail.com>
2021-04-19 17:26:13 +0100

    Change nav link color on background change

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -28,14 +28,14 @@ function Navigation(){
     window.addEventListener('scroll', changeNavbarBackground);
 
     return(
-        <Navbar fixed="top" bg={navbarChanged ? "customized": "transparent"} expand="lg" style={{paddingTop: '35px', zIndex: 1000}}>
+        <Navbar fixed="top" bg={navbarChanged ? "customized": "transparent"} expand="lg" style={{paddingTop: '10px', zIndex: 1000}}>
             <Container>
                 <Navbar.Brand href="#home" className={"brand-text"} style={{zIndex: -1}}>
                 <img
                     alt="Grachef Logo"
                     src="/images/logo.svg"
-                    width="30"
-                    height="30"
+                    width="50"
+                    height="50"
                     className="d-inline-block align-top"
                 />{' '}
                 Grachef
@@ -43,8 +43,8 @@ function Navigation(){
                 
                 {/* <Navbar.Collapse id="basic-navbar-nav"> */}
                     <Nav className="ml-auto nav-hack">
-                    <Nav.Link href="#cart" onClick={toggleCheckout}><FaShoppingCart className="navigation-link" /><sup><Badge pill className={'pill-style'}>276</Badge></sup></Nav.Link>
-                        <Nav.Link href="#menu"><FaBars className="navigation-link" /></Nav.Link>
+                    <Nav.Link href="#cart" onClick={toggleCheckout}><FaShoppingCart className={navbarChanged ? "navigation-link-customized" : "navigation-link"} /><sup><Badge pill className={'pill-style'}>276</Badge></sup></Nav.Link>
+                        <Nav.Link href="#menu"><FaBars className={navbarChanged ? "navigation-link-customized" : "navigation-link"} /></Nav.Link>
                     </Nav>
                 {/* </Navbar.Collapse> */}
             </Container>
